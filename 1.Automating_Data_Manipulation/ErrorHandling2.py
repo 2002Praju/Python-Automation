4d338369cc54c04d5bfb9732cc6e0fef733a2007
Prajwal Vyavahare <122688935+2002Praju@users.noreply.github.com>
2026-01-27 09:52:02 +0000

    The 3.Web_Browsing_Automation is not connected

--- 1.Automating_Data_Manipulation/ErrorHandling2.py
+++ 1.Automating_Data_Manipulation/ErrorHandling2.py
@@ -1,4 +1,4 @@
 years = [1925, 1943, 1968, 1937, 1975, 1912, 1989,1954, 1920, 1996]
-years.sort()
+years.reverse()   #Use sort method to tackle the error
 print(years)
 assert years[0] <= years[-1], "first element is greater than last element"
---
+++ 3.Web_Browsing_Automation/browser_automation.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+
+url ="https://vegamovies.paris/47517-the-tigers-apprentice-2024-hindi-dual-audio-web-dl-720p-480p-1080p.html"
